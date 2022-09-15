a33de56121072aa544c1341d17f764eeedc7978b
António Gonçalves <antonio.goncalves1995@hotmail.com>
2022-09-15 09:46:42 +0100

    Created dotfiles to help with git cli

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+GIT_ALIASES="
+[alias]
+    l = log
+    l = log
+    p = pull
+    st = status
+    co = checkout
+    br = branch
+    ci = commit
+    cp = cherry-pick
+    ap = add -p
+    aa = add -A
+    cm = commit -m
+    fixup = commit --amend --no-edit
+    cm = commit --message
+    cma = commit --message --all
+    cia = commit --amend
+    rc = rebase --continue
+    ra = rebase --abort
+    rh = reset --hard
+    pro = pull --rebase origin master
+"
+
+echo "$GIT_ALIASES" >> ~/.gitconfig
+
+git config --global push.default current
+
+cp ~/.dotfiles/.bash_aliases ~/.bash_aliases
